18d00095813f2f35977cbe579a1d349cae1af9d8
cssmason <shihsheng.mason.chang@gmail.com>
2022-02-12 15:18:33 +0800

    solution of verifying an alien dictionary with python

---
+++ python/0953-Verifying-an-Alien-Dictionary/VerifyinganAlienDictionary.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        dict = {value: index for index, value in enumerate(order)}
+        char2index = lambda word: [dict[char] for char in word]
+        return all(char2index(words[i]) <= char2index(words[i+1]) for i in range(len(words) - 1))
+
+if __name__ == '__main__':
+    words = ["hello", "leetcode"]
+    order = "hlabcdefgijkmnopqrstuvwxyz"
+    print(Solution().isAlienSorted(words, order))
