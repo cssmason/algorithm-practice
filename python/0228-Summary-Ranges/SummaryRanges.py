61ab6df25b79a246b615e1dda5b51b161052a408
cssmason <shihsheng.mason.chang@gmail.com>
2022-02-15 21:46:26 +0800

    solution of summary ranges with python

---
+++ python/0228-Summary-Ranges/SummaryRanges.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        start, ln = 0, len(nums)
+        for i in range(ln):
+            if i + 1 < ln and nums[i+1] == nums[i] + 1:
+                continue
+            if i == start:
+                result.append(str(nums[start]))
+            else:
+                result.append(f'{nums[start]}->{nums[i]}')
+            start = i + 1
+        return result
+
+if __name__ == '__main__':
+    nums = [0, 2, 3, 4, 6, 8, 9]
+    print(Solution().summaryRanges(nums))
