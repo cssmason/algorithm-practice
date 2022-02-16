f7a93c26bdb08b3f48ec5b81b2cbab608372f448
cssmason <shihsheng.mason.chang@gmail.com>
2022-02-16 22:59:09 +0800

    solution of repeated substring pattern with python

---
+++ python/0459-Repeated-Substring-Pattern/RepeatedSubstringPattern.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s*2)[1:-1]
+
+if __name__ == '__main__':
+    s = "abab"
+    print(Solution().repeatedSubstringPattern(s))
