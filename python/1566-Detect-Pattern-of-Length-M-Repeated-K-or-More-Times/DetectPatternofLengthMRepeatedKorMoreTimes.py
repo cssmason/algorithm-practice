3571a98b1635c7abc0a3211ff3901f0112b380bc
cssmason <shihsheng.mason.chang@gmail.com>
2022-02-18 17:26:59 +0800

    solution of detect pattern of length m repeated K or more times with python

---
+++ python/1566-Detect-Pattern-of-Length-M-Repeated-K-or-More-Times/DetectPatternofLengthMRepeatedKorMoreTimes.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+        streak = 0
+        for i in range(len(arr) - m):
+            if arr[i] == arr[i+m]:
+                streak += 1
+            else:
+                streak = 0
+            if streak == m * (k-1):
+                return True
+        return False
+
+if __name__ == '__main__':
+    arr = [1, 2, 4, 4, 4, 4]
+    m = 1
+    k = 3
+    print(Solution().containsPattern(arr, m, k))
