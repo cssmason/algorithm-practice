cd910bf10f2ac80a17143d5e187ed573df6eee05
cssmason <shihsheng.mason.chang@gmail.com>
2022-04-24 15:50:45 +0800

    solution of valid parentheses with python

---
+++ python/0020-Valid-Parentheses/ValidParentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+        for c in s:
+            if c in pairs:
+                if not stack or stack.pop() != pairs[c]:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
+      
+      
+if __name__ == '__main__':
+    s = "()[]{}"
+    print(Solution().isValid(s))
