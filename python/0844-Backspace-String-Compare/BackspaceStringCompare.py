7f19d5adef7f101927944868ae12865aabd685c3
cssmason <shihsheng.mason.chang@gmail.com>
2022-04-26 08:08:20 +0800

    solution of backspace string compare with python

---
+++ python/0844-Backspace-String-Compare/BackspaceStringCompare.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def build(S: str) -> list:
+            stack = []
+            for c in S:
+                if c != '#':
+                    stack.append(c)
+                elif stack:
+                    stack.pop()
+            return stack
+        return build(s) == build(t)
+
+if __name__ == '__main__':
+    s = "ab#c"
+    t = "ad#c"
+    print(Solution().backspaceCompare(s, t))
