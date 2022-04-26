4db51fa0e187bf645edf554602844fe346ae5c8f
cssmason <shihsheng.mason.chang@gmail.com>
2022-04-26 09:58:17 +0800

    solution of remove all adjacent duplicates in string with python

---
+++ python/1047-Remove-All-Adjacent-Duplicates-In-String/RemoveAllAdjacentDuplicatesInString.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if stack and stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+        return ''.join(stack)
+
+if __name__ == '__main__':
+    s = "abbaca"
+    print(Solution().removeDuplicates(s))
