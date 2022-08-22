fd336e09436f17717c6a1c994f4f802cc6d27349
cssmason <shihsheng.mason.chang@gmail.com>
2022-08-22 22:59:05 +0800

    solution of rotate list of list with python

---
+++ python/0061-Rotate-List/RotateList.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or k == 0:
+            return head
+        
+        curr, length = head, 0
+        while curr:
+            length += 1
+            prev, curr = curr, curr.next
+        else: 
+            prev.next = head
+
+        count = length - (k % length)
+
+        for _ in range(count):
+            prev, head = head, head.next
+        prev.next = None
+        return head
