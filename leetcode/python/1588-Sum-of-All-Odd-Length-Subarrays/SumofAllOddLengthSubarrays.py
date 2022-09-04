1ee73be449cd05a1b3c18677e83e5b3eec82a495
cssmason <shihsheng.mason.chang@gmail.com>
2022-09-04 22:27:12 +0800

    solution of sum of all odd length subarrays with python

---
+++ leetcode/python/1588-Sum-of-All-Odd-Length-Subarrays/SumofAllOddLengthSubarrays.py
@@ -0,0 +1,23 @@
+from typing import List
+class Solution:
+# # solution 1: running prefix sum - time complexity O(n^2)
+#     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+#         n = len(arr)
+#         ans = 0
+#         for i in range(n):
+#             sub = 0
+#             for j in range(i, n):
+#                 sub += arr[j] 
+#                 ans += sub if ((j - i + 1) % 2) == 1 else 0
+#         return ans
+
+# solution 2: math - time complexity O(n)
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        n, ans = len(arr), 0
+        for i in range(n):
+            ans += ((i + 1) * (n - i) + 1) // 2 * arr[i]
+        return ans
+
+if __name__ == '__main__':
+    arr = [1, 4, 2, 5, 3]
+    print(Solution().sumOddLengthSubarrays(arr))
