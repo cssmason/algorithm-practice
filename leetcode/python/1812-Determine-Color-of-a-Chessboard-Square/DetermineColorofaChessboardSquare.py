118f48c06b75ff684f8e23ad1cad7b26284420c8
cssmason <shihsheng.mason.chang@gmail.com>
2022-09-04 22:45:48 +0800

    solution of determine color of a chessboard square with python

---
+++ leetcode/python/1812-Determine-Color-of-a-Chessboard-Square/DetermineColorofaChessboardSquare.py
@@ -0,0 +1,7 @@
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        return sum(map(ord, coordinates)) % 2 != 0
+
+if __name__ == '__main__':
+    coordinates = "a1"
+    print(Solution().squareIsWhite(coordinates))
