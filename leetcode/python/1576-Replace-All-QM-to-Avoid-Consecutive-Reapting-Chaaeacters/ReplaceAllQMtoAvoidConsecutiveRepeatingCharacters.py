99c5b42d30603ee95bf72bfb65b5432a1b9b1350
cssmason <shihsheng.mason.chang@gmail.com>
2022-09-04 22:25:27 +0800

    solution of replace all s to avoid consecutive repeating characters with python

---
+++ leetcode/python/1576-Replace-All-QM-to-Avoid-Consecutive-Reapting-Chaaeacters/ReplaceAllQMtoAvoidConsecutiveRepeatingCharacters.py
@@ -0,0 +1,14 @@
+class Solution:
+    def modifyString(self, s: str) -> str:
+        s = list(s)
+        for i in range(len(s)):
+            if s[i] == '?':
+                for char in 'abc':
+                    if (i == 0 or s[i-1] != char) and (i == len(s) - 1 or s[i+1] != char):
+                        s[i] = char
+                        break
+        return ''.join(s)
+
+if __name__ == '__main__':
+    s = "?zs"
+    print(Solution().modifyString(s))
