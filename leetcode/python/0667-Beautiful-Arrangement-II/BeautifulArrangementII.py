f47749c668514e7b21759b01c14e32925198f157
cssmason <shihsheng.mason.chang@gmail.com>
2022-09-04 20:50:13 +0800

    solution of beautiful arrangement ii with python

---
+++ leetcode/python/0667-Beautiful-Arrangement-II/BeautifulArrangementII.py
@@ -0,0 +1,23 @@
+class Solution:
+    def constructArray(self, n: int, k: int) -> List[int]:
+        res = []
+        left, right = 1, n
+        for i in range(k):
+            if i % 2 == 0:
+                res.append(left)
+                left += 1
+            else:
+                res.append(right)
+                right -= 1
+        
+        if k % 2 == 0:
+            while left <= right:
+                res.append(right)
+                right -= 1
+        else:
+            while left <= right:
+                res.append(left)
+                left += 1
+        
+        return res
+                
