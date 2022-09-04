65476a275f4ac16271fb0b113d03857c2fee4b3a
cssmason <shihsheng.mason.chang@gmail.com>
2022-09-04 20:57:20 +0800

    solution of 132 pattern with python

---
+++ leetcode/python/0456-132-Pattern/132Pattern.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+        right, stack = float('-inf'), []
+        for num in reversed(nums):
+            if num < right:
+                return True
+            else:
+                while stack and stack[-1] < num:
+                    right = stack.pop()
+            stack.append(num)
+        return False
+        
+if __name__ == '__main__':
+    nums = [-1,3,2,0]
+    print(Solution().find132pattern(nums))
