13686207acbde0858e7125d634dd9631892e0a7c
cssmason <shihsheng.mason.chang@gmail.com>
2022-09-04 22:28:39 +0800

    solution of determine if string halves are alike with python

---
+++ leetcode/python/1704-Determine-if-String-Havles-Are-Alike/DetermineifStringHalvesAreAlike.py
@@ -0,0 +1,11 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        n, ans = len(s), 0
+        for i in range(n):
+            if s[i] in 'aeiouAEIOU':
+                ans += 1 if i < n // 2 else -1
+        return ans == 0
+
+if __name__ == '__main__':
+    s = "book"
+    print(Solution().halvesAreAlike(s))
