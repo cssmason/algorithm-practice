0bfec7461494933390f3ab03620891dc9dad7671
cssmason <shihsheng.mason.chang@gmail.com>
2022-09-10 10:48:00 +0800

    solution of largest time for given digits with python

---
+++ leetcode/python/0949-Largest-Time-for-Given-Digits/LargestTimeforGivenDigits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestTimeFromDigits(self, arr: List[int]) -> str:
+        arr.sort(reverse=True)
+        for i, j, m, n in itertools.permutations(arr):
+            if i * 10 + j < 24 and m * 10 + n < 60:
+                return '{}{}:{}{}'.format(i, j, m, n)
+        return ""
