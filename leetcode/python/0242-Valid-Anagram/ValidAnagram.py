79650b46e2166b2f7eb417ec342e97850b92fffd
cssmason <shihsheng.mason.chang@gmail.com>
2023-09-20 22:08:28 +0800

    solution of valid anagram with python

--- leetcode/python/0242-Valid-Anagram/ValidAnagram.py
+++ leetcode/python/0242-Valid-Anagram/ValidAnagram.py
@@ -1,7 +1,17 @@
-import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return collections.Counter(s) == collections.Counter(t)
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+        for index in range(len(s)):
+            countS[s[index]] = 1 + countS.get(s[index], 0)
+            countT[t[index]] = 1 + countT.get(t[index], 0)
+        for char in countS:
+            if countS[char] != countT.get(char, 0):
+                return False
+        return True
+
 
 if __name__ == '__main__':
     s = "anagram"
