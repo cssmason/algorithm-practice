96342604c063c9df795b5de4f447fcdd9da973bc
cssmason <shihsheng.mason.chang@gmail.com>
2023-09-21 22:54:50 +0800

    solution of two sum II with python

--- leetcode/python/0167-Two-Sum-II-Input-Array-Is-Sorted/TwoSumII.py
+++ leetcode/python/0167-Two-Sum-II-Input-Array-Is-Sorted/TwoSumII.py
@@ -5,12 +5,14 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while left < right:
-            if numbers[left] + numbers[right] > target:
+            curSum = numbers[left] + numbers[right]
+            if curSum > target:
                 right -= 1
-            elif numbers[left] + numbers[right] < target:
+            elif curSum < target:
                 left += 1
             else:
                 return [left+1, right+1]
+        return []
 
 
 if __name__ == '__main__':
