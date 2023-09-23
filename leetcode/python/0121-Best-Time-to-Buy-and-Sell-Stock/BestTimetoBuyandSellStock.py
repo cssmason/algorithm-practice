716fe0033d42e17f864b5c36d1783c196780768a
cssmason <shihsheng.mason.chang@gmail.com>
2023-09-23 09:04:40 +0800

    solution of best time to buy and sell stock with python

--- leetcode/python/0121-Best-Time-to-Buy-and-Sell-Stock/BestTimetoBuyandSellStock.py
+++ leetcode/python/0121-Best-Time-to-Buy-and-Sell-Stock/BestTimetoBuyandSellStock.py
@@ -1,14 +1,21 @@
 from typing import List
-import operator
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxEndingHere, maxSofFar = 0, 0
-        for profit in map(operator.sub, prices[1:], prices):
-            maxEndingHere = max(maxEndingHere + profit, profit)
-            maxSofFar = max(maxSofFar, maxEndingHere)
-        return maxSofFar
+        left, right = 0, 1
+        maxProfit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right += 1
+        return maxProfit
+
 
 if __name__ == '__main__':
     prices = [7, 1, 5, 3, 6, 4]
     print(Solution().maxProfit(prices))
-        
