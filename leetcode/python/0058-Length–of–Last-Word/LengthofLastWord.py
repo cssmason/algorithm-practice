9d92b32562e3d9ca88965d86a85c0f1c7559dec1
cssmason <shihsheng.mason.chang@gmail.com>
2023-09-30 09:04:00 +0800

    solution of length of last word with python

---
+++ leetcode/python/0058-Length–of–Last-Word/LengthofLastWord.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        index, length = len(s)-1, 0
+        while s[index] == " ":
+            index -= 1
+        while index >= 0 and s[index] != " ":
+            length += 1
+            index -= 1
+        return length
+
+
+if __name__ == '__main__':
+    s = "Hello World"
+    print(Solution().lengthOfLastWord(s))
