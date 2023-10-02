a3a17af59af8e18dc6b355db5a9d9caf278eae36
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-02 22:21:20 +0800

    solution of isomorphic strings with python

--- leetcode/python/0205-Isomorphic-Strings/IsomorphicStrings.py
+++ leetcode/python/0205-Isomorphic-Strings/IsomorphicStrings.py
@@ -1,11 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
+        # solution 1
+        mapST, mapTS = {}, {}
+        for char1, char2 in zip(s, t):
+            if ((char1 in mapST and mapST[char1] != char2) or
+               (char2 in mapTS and mapTS[char2] != char1)):
+                return False
+            mapST[char1] = char2
+            mapTS[char2] = char1
+        return True
+
+        # solution 2
+        # return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
+
 
 if __name__ == '__main__':
     s = "paper"
