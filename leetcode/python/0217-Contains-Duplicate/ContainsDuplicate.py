29df8a887435db39f20ab16e9d86e5ad2605db4b
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-03 21:32:27 +0800

    solution of contains duplicate with python

--- leetcode/python/0217-Contains-Duplicate/ContainsDuplicate.py
+++ leetcode/python/0217-Contains-Duplicate/ContainsDuplicate.py
@@ -1,7 +1,19 @@
 from typing import List
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        # solution 1:
+        # return len(nums) != len(set(nums))
+
+        # solution 2:
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
 
 if __name__ == '__main__':
     nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
