6d14ee9353a1c83c719c2adcc675b913a6113db8
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-05 12:09:48 +0800

    solution of majority element with python

--- leetcode/python/0169-Majority-Element/MajorityElement.py
+++ leetcode/python/0169-Majority-Element/MajorityElement.py
@@ -1,4 +1,6 @@
 from typing import List
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         major, count = None, 0
@@ -7,6 +9,7 @@ class Solution:
                 major = num
             count += (1 if num == major else -1)
         return major
+
 
 if __name__ == '__main__':
     nums = [2, 2, 1, 1, 1, 2, 2]
