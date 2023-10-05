2bdca1d8b9b40b2a1329371c02b688722a32e91c
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-05 12:20:20 +0800

    solution of implement stack using queues with python

--- leetcode/python/0225-Implement-Stack-using-Queues/ImplementStackusingQueues.py
+++ leetcode/python/0225-Implement-Stack-using-Queues/ImplementStackusingQueues.py
@@ -1,22 +1,25 @@
 from collections import deque
+
+
 class MyStack:
 
     def __init__(self):
-        self.queue= deque()
+        self.queue = deque()
 
     def push(self, x: int) -> None:
         self.queue.append(x)
-        for _ in range(len(self.queue) - 1):
-            self.queue.append(self.queue.popleft())
 
     def pop(self) -> int:
+        for _ in range(len(self.queue) - 1):
+            self.push(self.queue.popleft())
         return self.queue.popleft()
 
     def top(self) -> int:
-        return self.queue[0]
+        return self.queue[-1]
 
     def empty(self) -> bool:
-        return not self.queue
+        return len(self.queue) == 0
+
 
 if __name__ == '__main__':
     myStack = MyStack()
