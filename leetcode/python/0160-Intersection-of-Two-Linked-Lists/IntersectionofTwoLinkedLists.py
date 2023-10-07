8c29bfce3c53fbed4bbaea8051f4dbf512b2cd59
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-07 08:29:14 +0800

    solution of intersection of two linked lists with python

--- leetcode/python/0160-Intersection-of-Two-Linked-Lists/IntersectionofTwoLinkedLists.py
+++ leetcode/python/0160-Intersection-of-Two-Linked-Lists/IntersectionofTwoLinkedLists.py
@@ -8,6 +8,6 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         nodeA, nodeB = headA, headB
         while nodeA is not nodeB:
-            nodeA = headB if not nodeA else nodeA.next
-            nodeB = headA if not nodeB else nodeB.next
+            nodeA = nodeA.next if nodeA else headB
+            nodeB = nodeB.next if nodeB else headA
         return nodeA
