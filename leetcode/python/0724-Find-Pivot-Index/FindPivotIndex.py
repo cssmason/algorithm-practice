231b0d40cc4091393bb6859cdb3730db83c92fba
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-07 08:01:40 +0800

    solution of find pivot index with python

---
+++ leetcode/python/0724-Find-Pivot-Index/FindPivotIndex.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        leftSum = 0
+        for i in range(len(nums)):
+            rightSum = total - nums[i] - leftSum
+            if leftSum == rightSum:
+                return i
+            leftSum += nums[i]
+        return -1
+
+
+if __name__ == '__main__':
+    nums = [1, 7, 3, 6, 5, 6]
+    print(Solution().pivotIndex(nums))
