7701d706d3cba10ca80aac642c50c304d2792162
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-07 08:45:59 +0800

    solution of find all numbers disappeared in an array with python

--- leetcode/python/0448-Find-All-Numbers-Disappeared-in-an-Array/FindAllNumbersDisappearedinanArray.py
+++ leetcode/python/0448-Find-All-Numbers-Disappeared-in-an-Array/FindAllNumbersDisappearedinanArray.py
@@ -1,12 +1,25 @@
 from typing import List
+
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        result = []
-        unique = set(nums)
-        for i in range(1, len(nums)+1):
-            if i not in unique:
-                result.append(i)
-        return result
+        # solution 1:
+        # result = []
+        # unique = set(nums)
+        # for i in range(1, len(nums)+1):
+        #     if i not in unique:
+        #         result.append(i)
+        # return result
+        # solution 2:
+        for n in nums:
+            i = abs(n)-1
+            nums[i] = -1 * abs(nums[i])
+        res = []
+        for i, n in enumerate(nums):
+            if n > 0:
+                res.append(i+1)
+        return res
+
 
 if __name__ == '__main__':
     nums = [4, 3, 2, 7, 8, 2, 3, 1]
