28207464bc1a51de40cd74c8c5ffe31ce6c4cbcc
cssmason <shihsheng.mason.chang@gmail.com>
2023-10-14 15:43:52 +0800

    solution of maximum product subarray with python

--- leetcode/python/0152-Maximum-Product-Subarray/MaximumProductSubarray.py
+++ leetcode/python/0152-Maximum-Product-Subarray/MaximumProductSubarray.py
@@ -1,7 +1,10 @@
+from typing import List
+
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         res = max(nums)
-        curMin, curMax  = 1, 1
+        curMin, curMax = 1, 1
 
         for n in nums:
             if n == 0:
@@ -12,4 +15,8 @@ class Solution:
             curMax = max(tmp, curMax * n, n)
             res = max(res, curMax)
         return res
-            
+
+
+if __name__ == '__main__':
+    nums = [2, 3, -2, 4]
+    print(Solution().maxProduct(nums))
