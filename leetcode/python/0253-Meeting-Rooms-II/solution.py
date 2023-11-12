c7771cbdfdd9acd850b53a9d7274dd58891bed2f
cssmason <shihsheng.mason.chang@gmail.com>
2023-11-12 12:17:39 +0800

    solution of meeting rooms ii with python

---
+++ leetcode/python/0253-Meeting-Rooms-II/solution.py
@@ -0,0 +1,34 @@
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+
+    def minMeetingRooms(self, intervals):
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res, count = 0, 0
+        s, e = 0, 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
+
+
+if __name__ == '__main__':
+    intervals = []
+    intervals.append(Interval(0,30))
+    intervals.append(Interval(5,10))
+    intervals.append(Interval(15,20))
+    print(Solution().minMeetingRooms(intervals))
