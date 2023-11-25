cd7d56ad0c677e35e2996ead08b2e44cae69b653
cssmason <shihsheng.mason.chang@gmail.com>
2023-11-25 20:53:07 +0800

    solution of top k frequent elements with python

--- leetcode/python/0347-Top-K-Frequent-Elements/solution.py
+++ leetcode/python/0347-Top-K-Frequent-Elements/solution.py
@@ -1,18 +1,26 @@
-import collections
 from typing import List
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = collections.Counter(nums)
+        count = {}
         buckets = [[] for _ in range(len(nums) + 1)]
 
-        for key, value in counts.items():
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        for key, value in count.items():
             buckets[value].append(key)
 
         res = []
-        for i in range(len(nums), 0, -1):
+        for i in range(len(buckets) - 1, 0, -1):
             res += buckets[i]
             if len(res) == k:
                 return res
         return res
+
+
+if __name__ == '__main__':
+    nums = [1, 1, 1, 2, 2, 3]
+    k = 2
+    print(Solution().topKFrequent(nums, k))
