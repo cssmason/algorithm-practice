d7be08990712be784ee66d8388e0dac8a35b6511
cssmason <shihsheng.mason.chang@gmail.com>
2023-11-27 22:46:27 +0800

    solution of kth largest element in an array with python

--- leetcode/python/0215-Kth-Largest-Element-in-an-Array/solution.py
+++ leetcode/python/0215-Kth-Largest-Element-in-an-Array/solution.py
@@ -11,19 +11,19 @@ class Solution:
                 if nums[i] <= pivot:
                     nums[p], nums[i] = nums[i], nums[p]
                     p += 1
-                nums[p], nums[right] = nums[right], nums[p]
+            nums[p], nums[right] = nums[right], nums[p]
 
-                if p > k:
-                    return quickSelec(left, p - 1)
-                elif p < k:
-                    return quickSelect(p + 1, right)
-                else:
-                    return nums[p]
+            if p > k:
+                return quickSelect(left, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, right)
+            else:
+                return nums[p]
 
         return quickSelect(0, len(nums) - 1)
 
 
 if __name__ == '__main__':
-    nums = [3, 2, 1, 5, 6, 4]
-    k = 2
+    nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+    k = 4
     print(Solution().findKthLargest(nums, k))
